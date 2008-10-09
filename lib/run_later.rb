cb24671fbeb3d7309216d1e858e19bf1e0057b4c
Mathias Meyer <meyer@paperplanes.de>
2008-10-10 00:05:43 +0200

    Added a workaround for a problem that could occur when class caching is inactive, e.g. in development environment. The thread would crash because it references classes already unloaded by the dispatcher. The patch hooks into the dispatcher to check if the thread is still running, and holds of cleaning up until then.

--- lib/run_later.rb
+++ lib/run_later.rb
@@ -2,6 +2,18 @@ require 'run_later/worker'
 require 'run_later/instance_methods'
 
 ActionController::Base.send(:include, RunLater::InstanceMethods)
-ActiveRecord::Base.send(:include, RunLater::InstanceMethods)
 
-RunLater::Worker.new
+require 'dispatcher' unless defined?(::Dispatcher)
+
+RunLater::Worker.instance
+
+class ActionController::Dispatcher
+  def cleanup_application_with_thread_check
+    loop do
+      break unless RunLater::Worker.instance.thread[:running]
+    end
+    cleanup_application_without_thread_check
+  end
+
+  alias_method_chain :cleanup_application, :thread_check
+end
--- lib/run_later/worker.rb
+++ lib/run_later/worker.rb
@@ -1,24 +1,31 @@
 module RunLater
   class Worker
-    attr_accessor :logger
+    attr_accessor :logger, :thread
     
     def initialize(logger = RAILS_DEFAULT_LOGGER)
       @logger = logger
       @thread = Thread.new {
-        loop { 
+        loop {
           process_queue
         }
       }
+    end
+    
+    def self.instance
+      @worker ||= RunLater::Worker.new
     end
     
     def process_queue
       begin
         while block = RunLater.queue.pop
           Thread.pass
+          Thread.current[:running] = true
           block.call
+          Thread.current[:running] = false
         end
       rescue Exception => e
-        logger.error("Worker thread crashed, retrying.")
+        logger.error("Worker thread crashed, retrying. Error was: #{e}")
+        Thread.current[:running] = false
         retry
       end
     end
