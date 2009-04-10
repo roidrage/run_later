99138be71490d26139834769e703dabcf1a97947
Mathias Meyer <meyer@paperplanes.de>
2009-04-10 18:39:34 +0200

    Made run_later available in Observers as well. Thanks to Jonah Fox.

--- lib/run_later.rb
+++ lib/run_later.rb
@@ -6,3 +6,5 @@ ActionController::Base.send(:include, RunLater::InstanceMethods)
 # Make run_later available both as instance and class methods
 ActiveRecord::Base.send(:include, RunLater::InstanceMethods)
 ActiveRecord::Base.extend(RunLater::InstanceMethods)
+ActiveRecord::Observer.send(:include, RunLater::InstanceMethods)
+ActiveRecord::Observer.extend(RunLater::InstanceMethods)
